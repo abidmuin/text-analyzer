28b4fc83585faabe1e2ed3133902bfc0e635fed5
abidmuin <muin.739@gmail.com>
2025-06-14 21:38:35 +0600

    updated new schema for user and text

---
+++ prisma/migrations/20250614153734_init/migration.sql
@@ -0,0 +1,41 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT,
+    "name" TEXT,
+    "provider" TEXT NOT NULL,
+    "provider_id" TEXT NOT NULL,
+    "avatar_url" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "texts" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "char_count" INTEGER NOT NULL,
+    "word_count" INTEGER NOT NULL,
+    "sentence_count" INTEGER NOT NULL,
+    "paragraph_count" INTEGER NOT NULL,
+    "longest_words" TEXT[],
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "texts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_provider_provider_id_key" ON "users"("provider", "provider_id");
+
+-- CreateIndex
+CREATE INDEX "texts_user_id_idx" ON "texts"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "texts" ADD CONSTRAINT "texts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
