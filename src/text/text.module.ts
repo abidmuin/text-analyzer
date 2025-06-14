3f6df4bfe9d5ffc4ca528da45abd88a8e3cc13e2
abidmuin <muin.739@gmail.com>
2025-06-14 21:50:36 +0600

    feat: added PrismaService and AnalyzerService in text.module.ts

--- src/text/text.module.ts
+++ src/text/text.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TextService } from './text.service';
 import { TextController } from './text.controller';
+import { PrismaService } from '../prisma.service';
+import { AnalyzerService } from '../utils/analyzer.service';
 
 @Module({
-  controllers: [TextController],
-  providers: [TextService],
+  controllers: [TextController], providers: [TextService, PrismaService, AnalyzerService],
 })
-export class TextModule {}
+export class TextModule {
+}
