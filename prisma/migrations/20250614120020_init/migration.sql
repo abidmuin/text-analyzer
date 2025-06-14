28fa56b4cd41bc8f2638ab2376c720da55765604
abidmuin <muin.739@gmail.com>
2025-06-14 20:40:13 +0600

    deleted old db migration files

--- prisma/migrations/20250614120020_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Text" (
-    "id" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Text_pkey" PRIMARY KEY ("id")
-);
