0ee2ad9b514326c9c1b6144cd9d4be26edf71b3a
abidmuin <muin.739@gmail.com>
2025-06-14 17:37:28 +0600

    feat: added text resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TextModule } from './text/text.module';
 
 @Module({
-  imports: [],
+  imports: [TextModule],
   controllers: [AppController],
   providers: [AppService],
 })
